94e57e2f90dec90ceffa0787b57ff9e04560dcbc
Ross Murray <rm@egoorb.com>
2016-01-11 16:30:13 -0600

    update through part of tutorial 10 of 14

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemy : MovingObject
+{
+	public int PlayerDamage;
+
+	private Animator animator;
+	private Transform target;
+	private bool skipMove;
+	
+	protected override void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		throw new NotImplementedException();
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
 
 public class Player : MovingObject
 {
-	public int wallDamage = 1;
-	public int pointsPerFood = 10;
-	public int pointsPerSoda = 20;
-	public float restartLevelDelay = 1f;
+	public int WallDamage = 1;
+	public int PointsPerFood = 10;
+	public int PointsPerSoda = 20;
+	public float RestartLevelDelay = 1f;
 
-	private Animator Animator;
+	private Animator animator;
 	private int food;
 
 	protected override void Start()
 	{
-		Animator = GetComponent<Animator>();
-		food = GameManager.instance.playerFoodPoints;
+		animator = GetComponent<Animator>();
+		food = GameManager.Instance.PlayerFoodPoints;
 		base.Start();
 	}
 
 	private void OnDisable()
 	{
-		GameManager.instance.playerFoodPoints = food;
+		GameManager.Instance.PlayerFoodPoints = food;
 	}
 
-	protected override void AttemptMove <T>(int xDir, int yDir)
+	protected override void AttemptMove<T>(int xDir, int yDir)
 	{
 		food -= 1;
 		base.AttemptMove<T>(xDir, yDir);
+		RaycastHit2D hit;
+		if(Move(xDir, yDir, out hit))
+		{
+			//sfx
+		}
+		CheckIfGameOver();
+		GameManager.Instance.IsPlayersTurn = false;
 	}
 
 	private void CheckIfGameOver()
 	{
 		if(food <= 0)
 		{
-			GameManager.instance.GameOver();
+			GameManager.Instance.GameOver();
 		}
+	}
+
+	void Update()
+	{
+		if(!GameManager.Instance.IsPlayersTurn)
+		{
+			return;
+		}
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)Input.GetAxisRaw("Horizontal");
+		vertical = (int)Input.GetAxisRaw("Vertical");
+
+		if(horizontal != 0)
+		{
+			vertical = 0;
+		}
+		if(horizontal != 0 || vertical != 0)
+		{
+			AttemptMove<Wall>(horizontal, vertical);
+		}
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall(this.WallDamage);
+		animator.SetTrigger("playerChop");
+	}
+
+	private void OnTriggerEnter2d(Collider2D other)
+	{
+		if(other.tag == "Exit")
+		{
+			Invoke("Restart", RestartLevelDelay);
+			enabled = false;
+		}
+		else if(other.tag == "Food")
+		{
+			food += PointsPerFood;
+			other.gameObject.SetActive(false);
+		}
+		else if(other.tag == "Soda")
+		{
+			food += PointsPerSoda;
+			other.gameObject.SetActive(false);
+		}
+	}
+
+	private void Restart()
+	{
+		//SceneManager.LoadScene("Main");
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void LoseFood(int loss)
+	{
+		animator.SetTrigger("playerHit");
+		this.food -= loss;
+		CheckIfGameOver();
 	}
 }
